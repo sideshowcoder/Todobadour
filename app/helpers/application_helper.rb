f5547651ab7dcda3465fc8d24405cbe4b21786d7
Philipp Fehre <philipp.fehre@googlemail.com>
2012-02-23 15:22:02 +0100

    made title truely dynamic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  # Make the title configurable and available everywhere
+  def title
+    base_name = "Todobadour"
+    @title.nil? ? base_name : "#{base_name} : #{@title}"
+  end
+  
 end
