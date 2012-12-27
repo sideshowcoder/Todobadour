9b3d102468241b00c97dec3586cfb1f53e112ec5
Philipp Fehre <philipp.fehre@googlemail.com>
2012-12-27 12:22:53 +0100

    Moved todo features to rspec

--- spec/features/todo_edit_spec.rb
+++ spec/features/todo_edit_spec.rb
@@ -16,13 +16,25 @@ feature "edit todo" do
     expect(page).to have_content("✓")
   end
 
-  scenario "change title to valid title"
+  scenario "change title to valid title", js: true do
+    change_todo_title_for_todo("Changed", @todo)
+    expect(page).to have_content("Changed")
+  end
 
   scenario "change title to invalid title"
 
   def toggle_done_for_todo(todo)
     page.find("#todo-#{todo.id} span.done").click
     sleep 1
+  end
+
+  def change_todo_title_for_todo(title, todo)
+    within("#todo-#{todo.id}") do
+      page.find("[data-attribute=title]").click
+      fill_in "title", with: title
+      page.evaluate_script("$('#todo-#{todo.id} [data-attribute=title] form').submit()")
+      sleep 1
+    end
   end
 end
 
