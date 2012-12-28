b98ad3f285db238475141e8a6471df197b095c7e
Philipp Fehre <philipp.fehre@googlemail.com>
2012-12-28 13:12:11 +0100

    List styling

--- app/assets/javascripts/email_share_form.js
+++ app/assets/javascripts/email_share_form.js
@@ -1,11 +1,11 @@
 (function($){
 
   $(function(){
-    var email_share_form = $("#email_share_form");
+    var email_share_form = $("#email-share-form");
     var toggle_email_share_form = function(){
       email_share_form.toggle("fast");
     };
-    $("#email_share").on("click", toggle_email_share_form);
+    $("#email-share").on("click", toggle_email_share_form);
   });
 
 })(jQuery);
