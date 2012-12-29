f2be44a94f6743096101c6487d8dbfcd0c76de19
Philipp Fehre <philipp.fehre@googlemail.com>
2012-12-29 13:32:58 +0100

    Added by slug finder

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,27 +1,29 @@
 class List < ActiveRecord::Base
   extend FriendlyId
   friendly_id :slug
-  
-  attr_accessible :title, :description, :slug
-  
+
   has_many :todos, :dependent => :destroy
-  
+
   before_validation :generate_and_add_slug
-  
+
   validates :title, :presence => true,
                     :length => { :maximum => 140 }
-  
+
   validates :slug, :uniqueness => true,
                    :presence => true
-  
+
   def ordered_todos
     self.todos.rank(:position)
   end
-  
+
+  def self.by_slug(slug)
+    find(:first, conditions: { slug: slug })
+  end
+
   def share_via_email sender, receiver, message
     ShareMailer.share_list(self, sender, receiver, message).deliver
   end
-  
+
   private
     # generate a slug if none present before trying to validate
     def generate_and_add_slug
@@ -29,5 +31,5 @@ class List < ActiveRecord::Base
         self.slug = SecureRandom.hex 16
       end
       self
-    end                 
+    end
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -2,79 +2,42 @@ require 'spec_helper'
 require "faker"
 
 describe List do
-  
-  before(:each) do
-    slug = "bbq-#{rand 36**8}"
-    @attr = { :title => "Shopping for BBQ", 
-              :description => "Get everything for out great BBQ tonight", 
-              :slug => slug }
-  end
-  
-  it "should create a list with the given attributes" do
-    List.create! @attr
-  end
-  
-  it "should require a title" do
-    no_title_list = List.new @attr.merge :title => ""
-    no_title_list.should_not be_valid
-  end
-  
-  it "title should not be to long" do
-    too_long_title_list = List.new @attr.merge :title => "a" * 150
-    too_long_title_list.should_not be_valid
-  end
-    
-  it "should have a unique slug" do
-    List.create! @attr
-    duplicate_slug_list = List.new @attr
-    duplicate_slug_list.should_not be_valid
-  end
-  
+
   it "should generate a slug on save if none is present" do
-    list = List.create! @attr.merge :slug => ""    
-    list.slug.should_not be_blank
-    list = List.create!({ :title => "just a title" })
+    list = List.create!({ title: "just a title" })
     list.slug.should_not be_blank
   end
-    
+
+  it "should find a list by it's slug" do
+    list = FactoryGirl.create(:list)
+    List.by_slug(list.slug).should == list
+  end
+
   describe "todo association" do
-    
     before :each do
       @list = FactoryGirl.create :list
-      @t0 = FactoryGirl.create :todo, :list => @list, :created_at => 2.day.ago, :position => 1
-      @t1 = FactoryGirl.create :todo, :list => @list, :created_at => 1.day.ago, :position => 3
-      @t2 = FactoryGirl.create :todo, :list => @list, :created_at => 1.hour.ago, :position => 2
+      @t0 = FactoryGirl.create :todo, list: @list, created_at: 2.day.ago, position: 1
+      @t1 = FactoryGirl.create :todo, list: @list, created_at: 1.day.ago, position: 3
+      @t2 = FactoryGirl.create :todo, list: @list, created_at: 1.hour.ago, position: 2
     end
-    
+
     it "should return the todos in oder of order" do
       @list.todos.rank(:position).should == [@t0, @t2, @t1]
     end
-        
-    it "should destroy associated todos" do
-      @list.destroy
-      [@t1, @t2].each do
-        lambda do
-          Todo.find @t1 
-        end.should raise_error ActiveRecord::RecordNotFound
-      end
-    end
-    
   end
-  
+
   describe "share via email" do
-    
     before :each do
       @list = FactoryGirl.create :list
       @sender = "test@test.com"
       @receiver = "test2@test2.com"
-      @message = Faker::Lorem.paragraph 
+      @message = Faker::Lorem.paragraph
     end
-    
+
     it "should share itself via email" do
       @list.share_via_email @sender, @receiver, @message
       ActionMailer::Base.deliveries.last.to.should == [@receiver]
     end
-    
   end
 
 end
