c9b9ab50516a045812a899fe7bd014e1e2c7f1a0
Philipp Fehre <philipp.fehre@googlemail.com>
2012-12-30 13:52:56 +0100

    Fix up spec

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -6,17 +6,17 @@ describe ListsController do
 
     before(:each) do
       @list = FactoryGirl.create :list
-      @attr = { :title => "Changed #{@list.title}",
-                :description => "Changed #{@list.description}" }
+      @attr = { title: "Changed #{@list.title}",
+                description: "Changed #{@list.description}" }
     end
 
     it "should update title list" do
-      put :update, :id => @list, :list => @attr
+      put :update, id: @list, list: @attr
       List.find(@list).title.should == @attr[:title]
     end
 
     it "should redirect to the list page" do
-      put :update, :id => @list, :list => @attr
+      put :update, id: @list, list: @attr
       response.should redirect_to @list
     end
 
@@ -30,12 +30,12 @@ describe ListsController do
 
     it "should delete a list" do
       lambda do
-        delete :destroy, :id => @list
+        delete :destroy, id: @list
        end.should change(List, :count).by(-1)
     end
 
     it "should redirect to the list page" do
-      delete :destroy, :id => @list
+      delete :destroy, id: @list
       response.should redirect_to root_path
     end
 
@@ -44,18 +44,18 @@ describe ListsController do
   describe "GET 'show'" do
 
     before(:each) do
-      @list = List.create! :title => "Shopping for BBQ"
+      @list = List.create! title: "Shopping for BBQ"
     end
 
     it "should set a cookie" do
-      get :show, :id => @list
+      get :show, id: @list
       response.cookies['last_list'].should eq(@list.slug)
     end
 
     it "sets the cookie expiration" do
       stub_cookie_jar = HashWithIndifferentAccess.new
       controller.stub(:cookies) { stub_cookie_jar }
-      get :show, :id => @list
+      get :show, id: @list
       last_list_cookie = stub_cookie_jar['last_list']
       last_list_cookie[:expires].to_i.should be_within(1).of(14.day.from_now.to_i)
     end
