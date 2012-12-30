7dcaa94a0dd853b9142f3999decfaaea9b2454ad
Philipp Fehre <philipp.fehre@googlemail.com>
2012-12-30 13:47:13 +0100

    Implement search for slug

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,27 +1,33 @@
 class List < ActiveRecord::Base
   extend FriendlyId
   friendly_id :slug
-  
+
   attr_accessible :title, :description, :slug
-  
+
   has_many :todos, :dependent => :destroy
-  
+
   before_validation :generate_and_add_slug
-  
+
   validates :title, :presence => true,
                     :length => { :maximum => 140 }
-  
+
   validates :slug, :uniqueness => true,
                    :presence => true
-  
+
+  class << self
+    def for_slug(slug)
+      where(slug: slug).limit(1).first
+    end
+  end
+
   def ordered_todos
     self.todos.rank(:position)
   end
-  
+
   def share_via_email sender, receiver, message
     ShareMailer.share_list(self, sender, receiver, message).deliver
   end
-  
+
   private
     # generate a slug if none present before trying to validate
     def generate_and_add_slug
@@ -29,5 +35,5 @@ class List < ActiveRecord::Base
         self.slug = SecureRandom.hex 16
       end
       self
-    end                 
+    end
 end
